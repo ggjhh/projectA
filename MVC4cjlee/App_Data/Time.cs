40349a9e9bd0237b10e131671a608f7e714e2a80
이철준 <cjlee@wemakeprice.com>
2016-08-12 15:22:33 +0900

    솔루션 파일 추가, 파일 경로 이동

---
+++ MVC4cjlee/App_Data/Time.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MVC4cjlee.Utility
+{
+    /// <summary>
+    /// Time의 요약 설명입니다.
+    /// </summary>
+    public class Time
+    {
+        #region MySql UnixTime 변환
+        public static string ConvertToUnixTime(DateTime datetime)
+        {
+            DateTime sTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            long totalSeconds = (long)(datetime - sTime).TotalSeconds;
+            return totalSeconds.ToString();
+        }
+        #endregion
+    }
+}
---
+++ MVC4cjlee/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace MVC4cjlee.Controllers
+{
+    /// <summary>
+    /// 기본 Home
+    /// </summary>
+    /// <remarks>
+    ///     항상 ControllerBase 클래스를 상속 받는다. 
+    ///     전역으로 처리되어야 하는 기본 이벤트가 정의되어있다.
+    /// </remarks>
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
