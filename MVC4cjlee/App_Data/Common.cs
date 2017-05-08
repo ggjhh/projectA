53f02caa590bc4a2728937306d97780526e7136e
이철준 <cjlee@wemakeprice.com>
2017-05-08 12:48:52 +0900

    클래스,함수 추가. DB내용을 드롭다운리스트에 바인딩

--- MVC4cjlee/App_Data/Common.cs
+++ MVC4cjlee/App_Data/Common.cs
@@ -69,22 +69,61 @@ namespace MVC4cjlee.Utility
         }
 
 
-        public static class DBCommon
+    }
+    
+    public static class DBCommon
+    {
+        //DB 연결자 리턴
+        public static string DBBConnMsStr()
         {
-            //DB 연결자 리턴
-            public static string DBBConnMsStr()
-            {
-                string sSrv = "mssql" + MVC4cjlee.Web.Global.ServiceMode.ToString();
-                string connectionString = ConfigurationManager.ConnectionStrings[sSrv].ConnectionString;
-                return connectionString;
-            }
+            string sSrv = "mssql" + MVC4cjlee.Web.Global.ServiceMode.ToString();
+            string connectionString = ConfigurationManager.ConnectionStrings[sSrv].ConnectionString;
+            return connectionString;
+        }
 
-            public static string DBBConnMyStr()
-            {
-                string sSrv = "mysql" + MVC4cjlee.Web.Global.ServiceMode.ToString();
-                string connectionString = ConfigurationManager.ConnectionStrings[sSrv].ConnectionString;
-                return connectionString;
-            }
+        public static string DBBConnMyStr()
+        {
+            string sSrv = "mysql" + MVC4cjlee.Web.Global.ServiceMode.ToString();
+            string connectionString = ConfigurationManager.ConnectionStrings[sSrv].ConnectionString;
+            return connectionString;
         }
     }
+
+    #region DropDownList
+    public static class CommonDDL
+    {
+        public enum ePerPage
+        {
+            [Display(Name = "20개씩보기")]
+            twenty = 20,
+            [Display(Name = "30개씩보기")]
+            thirty = 30,
+            [Display(Name = "40개씩보기")]
+            fourty = 40,
+            [Display(Name = "50개씩보기")]
+            fifty = 50,
+            [Display(Name = "100개씩보기")]
+            hundred = 100,
+            [Display(Name = "150개씩보기")]
+            hundredfifty = 150
+        }
+        
+        #region DB 데이터를 DropDownList 바인딩
+        public static List<System.Web.Mvc.SelectListItem> getCodeList(DataTable ojbDT, bool isSelectDefault = false)
+        {
+            List<System.Web.Mvc.SelectListItem> items = new List<System.Web.Mvc.SelectListItem>();
+            items.Clear();
+            if(isSelectDefault)
+            {
+                items.Add(new System.Web.Mvc.SelectListItem { Text = "-선택-", Value = "" });
+            }
+            for (int intRowCount = 0; intRowCount <= ojbDT.Rows.Count - 1; intRowCount++)
+            {
+                items.Add(new System.Web.Mvc.SelectListItem { Text = ojbDT.Rows[intRowCount]["CODE_NAME"].ToString(), Value = ojbDT.Rows[intRowCount]["CODE_SEQ"].ToString() });
+            }
+            return items;
+        }
+        #endregion
+    }
+    #endregion
 }
