ed049eea5fcec26b3b998c60dbff04914aad639b
이철준 <cjlee@wemakeprice.com>
2017-05-08 12:58:27 +0900

    모델 추가. User 클래스

---
+++ MVC4cjlee/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC4cjlee.Models
+{
+    public class User
+    {
+        public string ID;
+        public string Name;
+        public string UserSeq;
+    }
+}
